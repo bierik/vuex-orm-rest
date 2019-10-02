2fccf292cd43046613162a8fede8068c0c875e13
SolStis86 <james@mvdigital.co>
2019-10-02 11:36:25 +0100

    Fix paths

--- src/save.js
+++ src/save.js
@@ -4,14 +4,13 @@ import joinPath from 'path.join';
 
 export default async function save(keys = Object.keys(this.$toJson()), { relations = [] } = {}) {
   const { post } = this.client;
-  const self = this;
 
   if (_.isUndefined(post)) {
     throw new Error('HTTP Client has no `post` method');
   }
 
   checkConstraints(this);
-  const path = joinPath(...relations.map(r => r.apiPath()), self.apiPath);
+  const path = joinPath(...relations.map(r => r.apiPath()), this.constructor.apiPath);
   const data = await post(path, this.pickKeys(keys));
   const stored = await this.$insert(data);
   return stored[this.constructor.entity][0];
--- src/update.js
+++ src/update.js
@@ -9,11 +9,9 @@ export default async function update(keys = Object.keys(this.$toJson()), { relat
     throw new Error('HTTP Client has no `patch` method');
   }
 
-  const self = this;
-
   checkConstraints(this);
 
-  const path = joinPath(...relations.map(r => r.apiPath()), self.apiPath);
+  const path = joinPath(...relations.map(r => r.apiPath()), this.apiPath());
   const { data } = await patch(path, this.pickKeys(keys));
   const stored = await this.$update(data);
   return stored[this.constructor.entity][0];
