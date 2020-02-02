c603b001c354fcf507f4ed9cce229654eb877f3c
SolStis86 <james@mvdigital.co>
2020-02-02 14:10:22 +0000

    change to insertOrUpdate on updated data

--- src/update.js
+++ src/update.js
@@ -13,6 +13,6 @@ export default async function update(keys = Object.keys(this.$toJson()), { relat
 
   const path = joinPath(...relations.map(r => r.apiPath()), this.apiPath());
   const { data } = await patch(path, this.pickKeys(keys));
-  const stored = await this.$update(data);
+  const stored = await this.$insertOrUpdate(data);
   return stored[this.constructor.entity][0];
 }
